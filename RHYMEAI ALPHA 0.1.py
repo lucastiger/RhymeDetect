b7b326383e96efa7694d290c15ed992215de65ef
lucastiger <116535626+lucastiger@users.noreply.github.com>
2022-10-25 17:42:46 -0700

    Update and rename RHYMEAI ALPHA 0.1.1.py to RHYMEAI ALPHA 0.1.py

--- RHYMEAI ALPHA 0.1.py
+++ RHYMEAI ALPHA 0.1.py
@@ -1,3 +1,4 @@
+import time
 
 def takeinput():
     global takeinput_permstorage_id
@@ -7,12 +8,12 @@ def takeinput():
     tempstorage_list = []
     n = 0
     while n == 0:
-        tempstorage_input = input('enter a rhyme word 1 to quit')
+        tempstorage_input = input('RhymeBot doesn\'t know much English. Enter a few words! Enter 1 to quit. ')
         if tempstorage_input == '1':
             n = 1
         else:
             tempstorage_list.append(tempstorage_input)
-    takeinput_permstorage_dict['permstorage_id' + str(takeinput_permstorage_id)]=tempstorage_list
+    takeinput_permstorage_dict['takeinput_permstorage_id' + str(takeinput_permstorage_id)]=tempstorage_list
     takeinput_permstorage_id +=1
     
 def checkrhyme():
@@ -27,7 +28,7 @@ def checkrhyme():
     global checkrhyme_permstorage_vals
     checkrhyme_permstorage_keys = list(takeinput_permstorage_dict.keys())
     checkrhyme_permstorage_vals = list(takeinput_permstorage_dict.values())
-    tempstorage_input = input('enter check rhyme word 1 to quit')
+    tempstorage_input = input('Enter a word that you want to check if it rhymes with the given data. Enter 1 to quit. ')
     tempstorage_list = []
     tempstoage_dict = {}
     n = 0
@@ -59,7 +60,7 @@ def checkrhyme():
                             tempstorage_list = [(checkrhyme_permstorage_vals[0])[i]]
                             while i < (tempstorage_listlen-1):
                                 if (tempstorage_ending in (checkrhyme_permstorage_vals[0])[i+1]):
-                                    tempstorage_list.append(tempstorage_ending)
+                                    tempstorage_list.append(checkrhyme_permstorage_vals[0][i+1])
                                     i += 1
                                 else:
                                     None
@@ -70,11 +71,12 @@ def checkrhyme():
                             print('position =', tempstorage_position)
                             print ('position id =', checkrhyme_permstorage_id)                                      #add cat identification
                             print ('rhyme part is', tempstorage_ending)
-                            print('matches with' s s)
+                            print('matches with' )
                             print (tempstorage_list)
                                                                                                                     #semantic - when joe, mow, low
                                                                                                                     #values and sow inputted,
                                                                                                                     #match result is ['mow', 'ow']
+                                                                                                                    ##SECTION FIXED
                                                                                                                     
                                                                                                                     
                                                                                                                     #add all rhyming, not just one
@@ -87,8 +89,52 @@ def checkrhyme():
                                                                                                                     #SECTION FIXED
                             n = 1
                             break
+                            
+                            
+def menu():
+    print('=========================')
                                                                                                                     
-
-takeinput()
-checkrhyme()
+def run():
+    print('EVOLVEAI INTRODUCES')
+    time.sleep(5)
+    print('RHYMEAI')
+    time.sleep(3)
+    print('SPONSERED BY LUCAS AIRLINES')
+    time.sleep(5)
+    print('ALL RIGHTS RESERVED TO EVOLVEAI COPYRIGHT 2022')
+    time.sleep(5)
+    print('Hey there! I\'m Rhymebot! ')
+    time.sleep(3)
+    print('Don\'t worry, I\'m friendly. ')
+    time.sleep(3)
+    print('How about you prove you are a human? I like humans. ')
+    time.sleep(3)
+    input('PROMPT: Press the Enter Key to prove to RhymeBot that you are a human. ')
+    time.sleep(2)
+    print('Great! I love humans! ')
+    time.sleep(2)
+    print("Why don't I show you around? Press the Enter Key again to get to the menu, where you'll find stuff to do! ")
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+         
+         
+         
+         
+       
+          
